540070a167ee4addab1aee20f599286019929826
tiia <tiia.ojares@gmail.com>
2023-05-23 14:50:51 +0300

    Post new Recipe to DB

--- recipesServer/src/main/java/com/recipes/controllers/RecipesController.java
+++ recipesServer/src/main/java/com/recipes/controllers/RecipesController.java
@@ -6,10 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -33,4 +30,11 @@ public class RecipesController {
         }
         return r;
     }
+
+    @PostMapping
+    Recipe create(@RequestBody Recipe newRecipe) {
+        recipeRepo.saveAndFlush(newRecipe);
+        return newRecipe;
+    }
+
 }
