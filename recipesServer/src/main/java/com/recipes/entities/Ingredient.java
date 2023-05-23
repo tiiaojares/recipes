fd2c31be8a436f1e5c045934463f79abd63a79a9
tiia <tiia.ojares@gmail.com>
2023-05-23 10:22:01 +0300

    Ingredient class added

---
+++ recipesServer/src/main/java/com/recipes/entities/Ingredient.java
@@ -0,0 +1,61 @@
+package com.recipes.entities;
+
+import jakarta.persistence.Column;
+
+public class Ingredient {
+    @jakarta.persistence.Id
+    private Integer Id;
+    @Column (name = "recipe_id")
+    private Integer recipeId;
+    @Column (name = "ingredient")
+    private String ingredient;
+    @Column (name = "amount")
+    private Integer amount;
+    @Column (name = "unit")
+    private String unit;
+
+
+    public Ingredient() {
+
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public Integer getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(Integer recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
