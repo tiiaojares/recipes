da0cbf7e82de5f89cd223fffdaf3edfb6c2e280c
tiia <tiia.ojares@gmail.com>
2023-05-24 11:25:12 +0300

    fixed test createRecipe_success

--- recipesServer/src/test/java/RecipesControllerTest.java
+++ recipesServer/src/test/java/RecipesControllerTest.java
@@ -13,10 +13,8 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
@@ -84,7 +82,6 @@ public class RecipesControllerTest {
                 .description("kuvaus4")
                 .build();
 
-        Mockito.when(recipeRepository.save(newRecipe)).thenReturn(newRecipe);
 
         String content = objectWriter.writeValueAsString(newRecipe);
 
