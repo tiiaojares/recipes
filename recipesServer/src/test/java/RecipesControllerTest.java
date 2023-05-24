72a51d98c973d57ec3052a92ce95a3576f15ca89
tiia <tiia.ojares@gmail.com>
2023-05-24 10:10:32 +0300

    added test getAllRecipes_success

---
+++ recipesServer/src/test/java/RecipesControllerTest.java
@@ -0,0 +1,65 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.recipes.controllers.RecipesController;
+import com.recipes.entities.Recipe;
+import com.recipes.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RecipesControllerTest {
+
+    private MockMvc mockMvc;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    ObjectWriter  objectWriter = objectMapper.writer();
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    @InjectMocks
+    private RecipesController recipesController;
+
+    Recipe recipe1 = new Recipe(1, "kuvaus1", "otsikko1");
+    Recipe recipe2= new Recipe(2, "kuvaus2", "otsikko2");
+    Recipe recipe3 = new Recipe(3, "kuvaus3", "otsikko3");
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(recipesController).build();
+    }
+
+    @Test
+    public void getAllRecipes_success() throws Exception {
+        List<Recipe> recipes = new ArrayList<>(Arrays.asList(recipe1, recipe2, recipe3));
+        Mockito.when(recipeRepository.findAll()).thenReturn(recipes);
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/recipes")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[2].title", is("kuvaus3")));
+    }
+
+}
