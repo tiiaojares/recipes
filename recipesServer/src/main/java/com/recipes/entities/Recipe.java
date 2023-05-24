6517e4a0d0cc0f83d9fe05e7f5bc3b5bc9019b45
tiia <tiia.ojares@gmail.com>
2023-05-24 10:45:51 +0300

    added @Builder annotation for testing

--- recipesServer/src/main/java/com/recipes/entities/Recipe.java
+++ recipesServer/src/main/java/com/recipes/entities/Recipe.java
@@ -6,7 +6,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import lombok.Builder;
 
+@Builder
 @Entity
 public class Recipe {
 
