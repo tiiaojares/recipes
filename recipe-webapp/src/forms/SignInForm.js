55d6c05f2944f8e5908cd24ddd2878c36c78205f
tiia <tiia.ojares@gmail.com>
2023-05-25 13:21:57 +0300

    added info wrong username or password

--- recipe-webapp/src/forms/SignInForm.js
+++ recipe-webapp/src/forms/SignInForm.js
@@ -7,36 +7,38 @@ const SignInForm = ({changeCreateNewAccount, setLogIn}) => {
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
     const [submit, changeSubmit] = useState(false);
+    const [wrongPassword, changeWrongPassword] = useState(false);
 
     function signIn() {
         changeSubmit(true);
         if (userName  && password) {
             axios.get("/user/login/"+userName).then(response => {
                 if (response.data === "") {
-                    console.log("väärä tunnus tai salasana")
+                    changeWrongPassword(true);
                 } else {
                     if (response.data.password === password) {
-                        console.log("sisään kirjaus onnistui: ", response.data)
+                        console.log("sisään kirjaus onnistui, tervetuloa ", response.data.name);
+                        setLogIn(true);
                     } else {
-                        console.log("väärä tunnus tai salasana")
+                        changeWrongPassword(true);
                     }       
                 }
             })
             changeSubmit(false);
-        } else {
-            console.log("email: ", userName)
-            console.log("password: ", password)
         }
     }
 
-    function createAccount() {
-        changeCreateNewAccount(true);
-    }
+
+    // function createAccount() {
+    //     changeCreateNewAccount(true);
+    // }
 
     return (
         <div className="signInComponent">
              <h2> Sign in: </h2>
+            
             <Form className="signInForm">
+            
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Username</Form.Label>
                     <Form.Control type="text" placeholder="Enter username" onChange={event => setUserName(event.target.value)}/>
@@ -58,10 +60,14 @@ const SignInForm = ({changeCreateNewAccount, setLogIn}) => {
                     : null
                     }
                 </Form.Group>
+                {wrongPassword && 
+                    <Form.Label style={{color:'red', textAlign:'left', marginBottom: '1rem'}}>
+                        Wrong username or password
+                    </Form.Label>}
                 <Button variant="success" onClick={() => signIn()}>
                     Submit
                 </Button>
-                <p>Or create a new account <u onClick={() => createAccount()}>here</u> </p>
+                <p>Or create a new account <u onClick={() => changeCreateNewAccount(true)}>here</u> </p>
             </Form>
         </div>
     )
