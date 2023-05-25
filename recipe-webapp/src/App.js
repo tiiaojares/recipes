78195d9098ccf4f83a26995239be3a2e1ed1d9c5
tiia <tiia.ojares@gmail.com>
2023-05-25 15:46:48 +0300

    deleted code that is not in use

--- recipe-webapp/src/App.js
+++ recipe-webapp/src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
-import { SignInForm } from './forms/SignInForm';
-import { NewAccountForm } from './forms/NewAccountForm';
-import Form from 'react-bootstrap/Form';
 import { LogInPage } from './LogInPage';
 
 
 
 const App = () => {
     const [logIn, setLogIn] = useState(false);
-
-    
-
-
-    // useEffect(() => {
-    //     axios.get("/recipes").then(response => {
-    //         console.log("recipes: ", response.data)
-    //     })
-    //     axios.get("/ingredients").then(response => {
-    //         console.log("ingredients: ", response.data)
-    //        })
-    // })
-
-   
-
+ 
 
     return (
         <div>
