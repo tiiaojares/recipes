2e636754d5a8e0a3c1a8f0708d7aeea68372b010
tiia <tiia.ojares@gmail.com>
2023-05-25 09:42:36 +0300

    functions getAll and create new user works

--- recipesServer/src/main/java/com/recipes/controllers/UserController.java
+++ recipesServer/src/main/java/com/recipes/controllers/UserController.java
@@ -1,12 +1,9 @@
 package com.recipes.controllers;
 
-import com.recipes.entities.Ingredient;
 import com.recipes.entities.User;
 import com.recipes.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +17,12 @@ public class UserController {
     @GetMapping
     List<User> getAll() {
         return userRepo.findAll();
+    }
+
+    @PostMapping
+    User create(@RequestBody User newUser) {
+        userRepo.saveAndFlush(newUser);
+        return newUser;
     }
 
 
