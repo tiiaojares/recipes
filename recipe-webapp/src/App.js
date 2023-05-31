7a1afbab8a6876c1bd45ac145dddac49ef0029ae
tiia <tiia.ojares@gmail.com>
2023-05-31 15:03:16 +0300

    Added Main component and Routes

--- recipe-webapp/src/App.js
+++ recipe-webapp/src/App.js
@@ -1,28 +1,31 @@
 
 import React from 'react';
 import {HashRouter as Router} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { useState } from 'react';
 import './App.css';
 import { LogInPage } from './LogInPage';
+import { NewAccountForm } from './forms/NewAccountForm';
 import { store } from './store.js';
 
 
 
+const Main = () => {
+    return (
+        <Routes>
+            <Route path="/"  element={<LogInPage />} />
+        </Routes>
+    )
+}
+
+
 const App = () => {
-    const [logIn, setLogIn] = useState(false);
 
 
     return (
         <Provider store={store} >
             <Router>
-                <div>
-                {!logIn ?
-                    <LogInPage setLogIn={setLogIn}/>
-                :
-                    <div> tervetuloa! </div>
-                }
-                </div> 
+                <Main /> 
             </Router>
         </Provider>
     );
