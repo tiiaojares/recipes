b5de44e137ba351fb8483c8f4598c3656402a909
tiia <tiia.ojares@gmail.com>
2023-05-31 22:07:42 +0300

    added navbar and dropdown to users site

--- recipe-webapp/src/pages/MainPage.js
+++ recipe-webapp/src/pages/MainPage.js
@@ -3,6 +3,9 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { setUser, store } from '../store';
 import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Dropdown from 'react-bootstrap/Dropdown';
+import '../App.css'
 
 const MainPage = () => {
     let user = useSelector(state => state.userReducer.user);
@@ -30,22 +33,42 @@ const MainPage = () => {
         
     } else {
         return (
-            
-            <div className="container"> 
-                <nav className="navbar">
+           
+            <div className="userBackroung">
+                <nav className="navbar navbar-right" style={{paddingTop: '1rem', paddingBottom: '1rem'}} >
+                    <div className="container">
+                        <div/>
+                        <h3 style={{color: 'green', fontFamily: 'serif', fontStyle: 'italic', marginBottom: '0'}}> My Own Recipes</h3>
+                            <Dropdown >
+                                <Dropdown.Toggle className="btn btn-success btn-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" classnAME="bi bi-list" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                                </svg> 
+                                    <div className="hideMobile" style={{display: 'inline'}}> Menu </div>
+                                </Dropdown.Toggle>
                 
-                <p 
-                   
-                    style={{textAlign:'center'}}>
-                    My Own Recepies
-                </p>
-                <Button
-                    onClick={() => logOut()}
-                    style={{textAlign: 'right'}}>
-                    Log out
-                </Button>
-            
-                </nav>    
+                                <Dropdown.Menu>
+                                    <Dropdown.Item  
+                                        className="btn btn-success btn-sm" 
+                                        > Search...
+                                    </Dropdown.Item>
+                                    <Dropdown.Item  
+                                        className="btn btn-success btn-sm" 
+                                        > Create new 
+                                    </Dropdown.Item>
+                                    <Dropdown.Item  
+                                        className="btn btn-success btn-sm" 
+                                        onClick={() => navigate("/")}>Log out
+                                    </Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                    </div>   
+
+                </nav> 
+                <div>
+                    <div className="container"> 
+                    </div>
+                </div>
             </div>
         )
     }
