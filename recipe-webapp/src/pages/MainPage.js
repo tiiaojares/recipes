108e56cae3e92087aa6dbba9a37b20410b73ac32
tiia <tiia.ojares@gmail.com>
2023-05-31 17:20:24 +0300

    added logOut button

--- recipe-webapp/src/pages/MainPage.js
+++ recipe-webapp/src/pages/MainPage.js
@@ -1,6 +1,8 @@
 
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { setUser, store } from '../store';
+import Button from 'react-bootstrap/Button';
 
 const MainPage = () => {
     let user = useSelector(state => state.userReducer.user);
@@ -9,13 +11,42 @@ const MainPage = () => {
 
     const navigate = useNavigate();
 
+    function logOut() {
+        store.dispatch(setUser({
+            id: 0, 
+            name: '',
+            username: '',
+            password: '',
+        }));
+        console.log("log out, id: ", id)
+
+        navigate("/");
+    }
 
 
     if (id === 0) {
         navigate("/")
+        return;
+        
     } else {
         return (
-            <div> Welcome </div>
+            
+            <div className="container"> 
+                <nav className="navbar">
+                
+                <p 
+                   
+                    style={{textAlign:'center'}}>
+                    My Own Recepies
+                </p>
+                <Button
+                    onClick={() => logOut()}
+                    style={{textAlign: 'right'}}>
+                    Log out
+                </Button>
+            
+                </nav>    
+            </div>
         )
     }
     
