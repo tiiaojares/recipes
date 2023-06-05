bcaacd6cf054ea6a2603ff55ceb8fa8bd029ec5a
tiia <tiia.ojares@gmail.com>
2023-06-05 15:28:32 +0300

    added route recipes/:id

--- recipe-webapp/src/App.js
+++ recipe-webapp/src/App.js
@@ -6,9 +6,10 @@ import { Provider } from 'react-redux';
 import './App.css';
 import { LogInPage } from './pages/LogInPage';
 import { store } from './store.js';
-import { MainPage } from './pages/MainPage'
 import { CreateNewRecipe } from './pages/CreateNewRecipe';
 import { Profile } from './pages/Profile';
+import { Recipes } from './pages/Recipes';
+import { SelectedRecipe } from './pages/SelectedRecipe';
 
 
 
@@ -16,9 +17,10 @@ const Main = () => {
     return (
         <Routes>
             <Route path="/"  element={<LogInPage />} />
-            <Route path="/recipes" element={<MainPage /> } />
+            <Route path="/recipes" element={<Recipes /> } />
             <Route path="/new_recipe" element={<CreateNewRecipe />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/recipes/:id" element={<SelectedRecipe />} />
         </Routes>
     )
 }
