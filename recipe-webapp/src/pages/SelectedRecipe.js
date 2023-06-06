14c3692bc02f89d5a04f2418c4be051798083b49
tiia <tiia.ojares@gmail.com>
2023-06-06 10:03:55 +0300

    selected recipe view shows title and description

--- recipe-webapp/src/pages/SelectedRecipe.js
+++ recipe-webapp/src/pages/SelectedRecipe.js
@@ -6,27 +6,36 @@ import recipeService from '../services/recipe';
 
 const SelectedRecipe = () => {
     const [recipe, setRecipe] = useState();
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
 
     let params = useParams();
     let id = params.id;
+      
 
     useEffect(() => {
         recipeService
             .getRecipe(id)
             .then(response => {
                 setRecipe(response.data);
+                setTitle(response.data.title);
+                setDescription(response.data.description);
                 console.log("selected resipe: ", response.data)
             })
     }, [id]);
 
-    return (
-        <div className="userBackroung">
-            <Navbar text={"Omat reseptit"}/>
-            <div className="container" >
-                
+        return (
+            <div className="userBackroung">
+                <Navbar text={"Omat reseptit"}/>
+                <div className="container" >
+                    <div className="note" >
+                        <h1 style={{textAlign: 'center', paddingTop: '1rem', color: '#606060'}}> { title} </h1>
+                        <p style={{textAlign: 'center', fontStyle: 'italic', padding: '0.3rem'}}> { description } </p>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    
 }
 
 export { SelectedRecipe }
